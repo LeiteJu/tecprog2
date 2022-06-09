4910719548e3212389c709aed1414684c1f7f449
LeiteJu <julia.lsilva724@gmail.com>
2022-06-09 00:15:26 -0300

    Primeiro teste: se um ano divisível por 4 é identificado como bissexto

---
+++ miniep8/bissexto.py
@@ -0,0 +1,15 @@
+''' 
+Programa que recebe um ano e devolve True se o ano for bissexto
+e False caso contrario...
+
+Um ano e bissexto se for divisivel por 4, mas nao divisivel por 100, 
+exceto se ele for tambem divisivel por 400
+
+'''
+
+# funcao que verifica se o ano e bissexto
+def check (ano):
+
+    # ano divisivel por 4
+    return ano % 4 == 0
+
---
+++ miniep8/test_cases.py
@@ -0,0 +1,5 @@
+import bissexto
+
+def teste_1 ():
+    assert bissexto.check(2020) == True
+
