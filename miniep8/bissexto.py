6b81fc478096e9fbf17a5a6f09e3de92cea916d5
LeiteJu <julia.lsilva724@gmail.com>
2022-06-09 00:21:13 -0300

    Segundo teste, agora verificando se um ano múltiplo de 4 e de 100 é identificado como não bissexto

--- miniep8/bissexto.py
+++ miniep8/bissexto.py
@@ -10,6 +10,8 @@ exceto se ele for tambem divisivel por 400
 # funcao que verifica se o ano e bissexto
 def check (ano):
 
-    # ano divisivel por 4
-    return ano % 4 == 0
+    # ano divisivel por 4 mas nao por 100
+    if ano % 4 == 0 and ano % 100 != 0:
+        return True
 
+    return False
--- miniep8/test_cases.py
+++ miniep8/test_cases.py
@@ -3,3 +3,6 @@ import bissexto
 def teste_1 ():
     assert bissexto.check(2020) == True
 
+def teste_2 ():
+    assert bissexto.check(1800) == False
+
