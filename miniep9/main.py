72ffe5a1985643a6b47f20f3ec453c8045e81852
LeiteJu <julia.lsilva724@gmail.com>
2022-07-02 16:38:39 -0300

    varios

--- miniep9/main.py
+++ miniep9/main.py
@@ -5,7 +5,15 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from selenium.webdriver.chrome.options import Options
 
-if __name__ == "__main__":
+import pytest
+
+
+def test_kerberos ():
+
+    # options = Options()
+
+    # options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
 
     driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -15,17 +23,27 @@ if __name__ == "__main__":
     assert "Let Me Google That" in driver.title
     print("Acessamos o: " + driver.title + "\n")
 
+    # assert "Python" in driver.title
     elem = driver.find_elements(By.ID, "search-input")
+    # print(elem)
+    # elem.clear()
     text_box = elem[0] 
-    text_box.send_keys("O que e Kerberos?")
+    text_box.send_keys("O que é Kerberos?")
     
     # generate the link
     but = driver.find_elements(By.ID, "search")[0].click()
+
+    # sleep(15)
 
     url = driver.find_elements(By.ID, "url-result")
 
     assert url != []
 
     print(url[0].get_attribute('value'))
+
+    # sleep(15)
+    # elem.send_keys(Keys.RETURN)
+    # assert "No results found." not in driver.page_source
+    # print(x)
 
     driver.quit()
